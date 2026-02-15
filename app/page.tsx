efa6e857316cd2e372886509c7d3bf0d3995b2b6
Mahi <hero90960@gmail.com>
2026-02-15 17:28:02 +0600

    Seting up Next-js

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1>
+      Hello filesystem! This is a test of the new filesystem API in Next.js 14. You can read more about it in the documentation: https://nextjs.org/docs/app/building-your-application/routing#filesystem-api
+    </h1>
+  );
+}
